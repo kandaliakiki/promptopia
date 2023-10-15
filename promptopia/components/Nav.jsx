e164e5dc302a853e8e6c3fc51cc72e5dc9bed873
kandaliakiki <kandaliak1k1@gmail.com>
2023-10-15 07:50:04 +0700

    smpe bikin navbar

---
+++ promptopia/components/Nav.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useSession, signIn, signOut, getProviders } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+
+const Nav = () => {
+  return <div>Nav</div>;
+};
+
+export default Nav;
