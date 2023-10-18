9d0a1d3bf270697cb096c6ab88a188298fbe3f0f
kandaliakiki <kandaliak1k1@gmail.com>
2023-10-18 12:09:03 +0700

    craete nav bar  including responsive navbar and dropdown for responsive navbar

--- promptopia/app/page.jsx
+++ promptopia/app/page.jsx
@@ -6,7 +6,7 @@ const Home = () => {
       <h1 className="head_text text-center">
         Discover & Share
         <br className="max-md:hidden"></br>
-        <span className="orange_gradient text-center">Ai-Powered Prompts</span>
+        <span className="orange_gradient text-center"> Ai-Powered Prompts</span>
       </h1>
       <p className="desc text-center">
         Promptopia is an open-source AI prompting tool for modern world to
