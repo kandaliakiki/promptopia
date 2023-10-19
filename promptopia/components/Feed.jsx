6e8c5104fe0b263b49ce7f4b51f57613f50109aa
kandaliakiki <kandaliak1k1@gmail.com>
2023-10-19 16:21:08 +0700

    getting prompt from database and showing it onto promptcard on home

--- promptopia/components/Feed.jsx
+++ promptopia/components/Feed.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+"use client";
+
+import { useEffect, useState } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        ></PromptCard>
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+  const handleSearchChange = (e) => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      console.log("datanya " + JSON.stringify(data[0]));
+      setPosts(data);
+    };
+
+    fetchPosts();
+    console.log("postnya : " + posts[0]);
+  }, []);
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        ></input>
+      </form>
+
+      <PromptCardList data={posts} handleTagClick={() => {}}></PromptCardList>
+    </section>
+  );
 };
 
 export default Feed;
