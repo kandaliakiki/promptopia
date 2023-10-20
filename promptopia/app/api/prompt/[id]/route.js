35fbd8759f46e0ed57b7b3a68b815549ff4a8149
kandaliakiki <kandaliak1k1@gmail.com>
2023-10-20 19:39:33 +0700

    siapin backend utk smua api (put , post , patch ,delete)

---
+++ promptopia/app/api/prompt/[id]/route.js
@@ -0,0 +1,58 @@
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+
+// GET (read)
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDB();
+
+    // const prompts = await Prompt.find({ _id: params.id }).populate("creator");
+    const prompts = await Prompt.findById(params.id).populate("creator");
+
+    if (!prompt) return new Response("Prompt not found", { status: 404 });
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch all Prompts", { status: 500 });
+  }
+};
+
+// PATH (update)
+export const PATCH = async (req, { params }) => {
+  const { prompt, tag } = await req.json();
+  try {
+    await connectToDB();
+    const existingPrompt = await Prompt.find(params.id);
+    if (!existingPrompt)
+      return new Response("Prompt not found ", { status: 404 });
+
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+    await existingPrompt.save();
+
+    return new Response(JSON.stringify(existingPrompt), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to update Prompt", { status: 500 });
+  }
+};
+
+// DELETE (delete)
+export const DELETE = async (req, { params }) => {
+  //   const { prompt, tag } = req.json();
+
+  try {
+    await connectToDB();
+
+    // const existingPrompt = await Prompt.findById(params.id);
+    // if (!existingPrompt)
+    //   return new Response("Failed to find prompt", { status: 404 });
+
+    // await existingPrompt.delete();
+
+    await Prompt.deleteOne({ _id: params.id });
+
+    // await Prompt.findByIdAndRemove(params.id);
+    return new Response("Prompt deleted Successfully ", { status: 200 });
+  } catch (error) {
+    return new Response("Failed to delete prompt ", { status: 500 });
+  }
+};
