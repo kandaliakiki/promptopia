d14d12a15dc5e3cc5951f3d87a77fe6bb15e1327
kandaliakiki <kandaliak1k1@gmail.com>
2024-05-07 08:49:33 +0700

    use port from env

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
+  server: {
+    port: parseInt(process.env.PORT, 10) || 3000,
+  },
   webpack(config) {
     config.experiments = {
       ...config.experiments,
